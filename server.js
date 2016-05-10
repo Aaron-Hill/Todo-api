7a59b1ae61c7b5f57fa5f50f285adc4ee3e9f2ed
Aaron Hill <Finch659@gmail.com>
2016-05-10 01:22:33 -0400

    Add ?completed filter

--- server.js
+++ server.js
@@ -9,13 +9,23 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
+//GET ROOT
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true
 app.get('/todos', function(req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
 //GET /todos/:id Check out underscore docs
@@ -81,7 +91,6 @@ app.put('/todos/:id', function(req, res) {
 		return res.status(400).send();
 	}
 
-	//HERE
 	_.extend(matchedTodo, validAttributes);
 	res.json(matchedTodo);
 
