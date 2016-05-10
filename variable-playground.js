a90ed47f7995cd6adfadde85fb5c15acdc79bd1f
Aaron Hill <Finch659@gmail.com>
2016-05-10 01:23:14 -0400

    Add variable playgound area

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+// var person = {
+// 	name: 'Aaron',
+// 	age: 234
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Aaron',
+// 	// 	age: 26
+// 	// };
+// 	obj.age = 26;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+//Array example
+
+var grades = [15, 37];
+
+function addgrade (arr) {
+	arr.push(23);
+	debugger;
+
+	// arr = [12, 33, 99];
+}
+
+addgrade(grades);
+console.log(grades);
